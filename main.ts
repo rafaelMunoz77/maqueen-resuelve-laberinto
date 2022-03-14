f61f4ffdad1b47377f1d9a2198d17ece0a14147c
rafaelMunoz77 <32701274+rafaelMunoz77@users.noreply.github.com>
2022-03-14 11:40:46 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+function girar90gradosDerecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    basic.pause(1100)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function girar180gradosDerecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    basic.pause(200)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function girar90gradosIzquierda () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+    basic.pause(1100)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+let ultimoGiro = 0
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        girar90gradosIzquierda()
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    }
+})
